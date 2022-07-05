9ffb762ae73eda2a984af932700551342b608687
= <=>
2022-07-05 10:13:00 +0700

    custom UI homepage

--- src/layouts/components/Content/index.js
+++ src/layouts/components/Content/index.js
@@ -1,7 +1,86 @@
-import VideoInfo from './VideoInfo';
+import Image from '~/components/Image';
+import classNames from 'classnames/bind';
+import Styles from './Content.module.scss';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faMusic } from '@fortawesome/free-solid-svg-icons';
+import Button from '~/components/Button';
+import { HeartIcon, CommentIcon, ShareIcon } from '~/components/Icons';
+import { useRef, useState } from 'react';
+import video from '~/assets/video/tải xuống.mp4';
 
-function Content() {
-  return <VideoInfo />;
+const cx = classNames.bind(Styles);
+
+function VideoInfo() {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+  return (
+    <div className={cx('wrapper')}>
+      <div className={cx('video-info')}>
+        <div className={cx('user-info')}>
+          <Link to={'/@vinhss00'}>
+            <Image
+              src="https://files.fullstack.edu.vn/f8-prod/user_photos/214474/62b117e56e1c9.jpg"
+              alt="Vinhss00"
+              className={cx('user-avatar')}
+            />
+          </Link>
+          <div>
+            <Link className={cx('info')} to={'/@vinhss00'}>
+              <h4 className={cx('name')}>
+                <span>Vinhss00</span>
+                <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+              </h4>
+              <span className={cx('username')}>Vinhss00</span>
+            </Link>
+            <div className={cx('decription')}>
+              <span className={cx('content-post')}>vinh sô up tiktok nè</span>
+              <div>
+                <FontAwesomeIcon icon={faMusic} />
+                <span className={cx('info-song')}>bài hát đang phát</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Button className={cx('btn')} outline>
+          Following
+        </Button>
+      </div>
+      <div className={cx('container')}>
+        <video ref={videoRef} onClick={onVideoPress} className={cx('video')} src={video} loop />
+        <div className={cx('action')}>
+          <button className={cx('reaction')}>
+            <span className={cx('icon')}>
+              <HeartIcon />
+            </span>
+            <span className={cx('count')}>100k</span>
+          </button>
+          <button className={cx('reaction')}>
+            <span className={cx('icon')}>
+              <CommentIcon />
+            </span>
+            <span className={cx('count')}>10k</span>
+          </button>
+          <button className={cx('reaction')}>
+            <span className={cx('icon')}>
+              <ShareIcon />
+            </span>
+            <span className={cx('count')}>13,4k</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Content;
+export default VideoInfo;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,23 @@
+import { useEffect, useRef } from 'react';
 import Content from '~/layouts/components/Content';
+import classNames from 'classnames/bind';
+import Styles from './Home.module.scss';
+
+const cx = classNames.bind(Styles);
 
 function Home() {
-  return <Content />;
+  const contentRef = useRef();
+  useEffect(() => {
+    contentRef.current.focus();
+  }, []);
+  return (
+    <div ref={contentRef} className={cx('wrapper')}>
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+    </div>
+  );
 }
 
 export default Home;
